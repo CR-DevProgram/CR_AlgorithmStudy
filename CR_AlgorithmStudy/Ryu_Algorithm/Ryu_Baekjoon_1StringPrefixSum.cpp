2a355cf1f7ab85d50891ef91e993bfe92d1890cf
Rotus <12rotus31@gmail.com>
2024-04-09 11:43:28 +0900

    Ryu : 백준 1.문자열,누적합(일곱 난쟁이_조합)

--- CR_AlgorithmStudy/Ryu_Algorithm/Ryu_Baekjoon_1StringPrefixSum.cpp
+++ CR_AlgorithmStudy/Ryu_Algorithm/Ryu_Baekjoon_1StringPrefixSum.cpp
@@ -51,35 +51,90 @@ using namespace std;
 // 9개 중 7개 뽑는다, 순서와 상관이 없다 9C7
 // 그런데 이 문제는 9P7로도 뽑을 수 있다 Permutation은 순서와 상관있지만 n(== 9)의 수 자체가 작기 때문에 가능
 // 9P7 => 9! / 2! => 9 * 8 * 7 * 6 * 5 * 4 * 3 => 181440
+//int arr[9];
+//
+//int main()
+//{
+//	// 아홉 난쟁이의 키 입력 받기
+//	for (int i = 0; i < 9; ++i)
+//	{
+//		cin >> arr[i];
+//	}
+//
+//	// 순열을 할 때는 항상 정렬. 오름차순으로 해줘야 함
+//	sort(arr, arr + 9);
+//
+//	do
+//	{
+//		// 디버깅 코드
+//		for (int i : arr) cout << i << " ";
+//		cout << "\n";
+//
+//		// 로직
+//		// Sum을 0으로 계속 초기화
+//		int Sum = 0;
+//		// 일곱 난쟁이의 합
+//		for (int i = 0; i < 7; ++i) Sum += arr[i];
+//		// 합이 100일 때 멈춤
+//		if (100 == Sum) break;
+//	} while (next_permutation(arr, arr + 9));
+//
+//	for (int i = 0; i < 7; ++i) cout << arr[i] << "\n";
+//
+//	return 0;
+//}
+
+// 또 다른 풀이법(조합)
+// 9C7은 9C2와 같다
+// 정상적인 일곱 난쟁이를 뽑는 것과 비정상적인 두 난쟁이를 뽑아내는 것은 결국 같은 로직이나 다름없다
+// 100 + A + B가 아홉 난쟁이의 합일 것. 그 중 A와 B를 찾아낸다는 것은 100 == 전체 - (A + B)
+
+// 2개 뽑이라 pair 사용
+pair<int, int> ret;
+vector<int> vec;
 int arr[9];
+int Sum;
+
+void Solve()
+{
+	// 9C2이니까 중첩for문은 2번만
+	for (int i = 0; i < 9; ++i)
+	{
+		for (int j = 0; j < i; ++j)
+		{
+			// 로직
+			if (100 == Sum - (arr[i] + arr[j]))
+			{
+				ret = { i, j };
+				return;
+			}
+		}
+	}
+}
 
 int main()
 {
-	// 아홉 난쟁이의 키 입력 받기
 	for (int i = 0; i < 9; ++i)
 	{
+		// 입력 받기
 		cin >> arr[i];
+		// 전체 합 구하기
+		Sum += arr[i];
+	}
+	
+	Solve();
+
+	for (int i = 0; i < 9; ++i)
+	{
+		// 가짜 난쟁이 둘 중 하나에 해당 할 경우 건너 뛴다 == continue
+		if (ret.first == i || ret.second == i) continue;
+		// 진짜 난쟁이 일곱만 벡터에 넣어줌
+		vec.push_back(arr[i]);
 	}
 
-	// 순열을 할 때는 항상 정렬. 오름차순으로 해줘야 함
-	sort(arr, arr + 9);
-
-	do
-	{
-		// 디버깅 코드
-		for (int i : arr) cout << i << " ";
-		cout << "\n";
-
-		// 로직
-		// Sum을 0으로 계속 초기화
-		int Sum = 0;
-		// 일곱 난쟁이의 합
-		for (int i = 0; i < 7; ++i) Sum += arr[i];
-		// 합이 100일 때 멈춤
-		if (100 == Sum) break;
-	} while (next_permutation(arr, arr + 9));
-
-	for (int i = 0; i < 7; ++i) cout << arr[i] << "\n";
+	// 오름차순 출력을 위한 정렬
+	sort(vec.begin(), vec.end());
+	for (int i : vec) cout << i << "\n";
 
 	return 0;
 }
