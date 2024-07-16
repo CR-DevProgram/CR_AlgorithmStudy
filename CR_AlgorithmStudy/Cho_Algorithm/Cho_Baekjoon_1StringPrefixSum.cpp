e450cee7d926fe4015bef9d0ea038b8cd50af58d
ChowonKim <97011721+chowon986@users.noreply.github.com>
2024-07-16 17:32:10 +0900

    Cho : 백준 1.문자열,누적합(팰린드롬인지 확인하기)

--- CR_AlgorithmStudy/Cho_Algorithm/Cho_Baekjoon_1StringPrefixSum.cpp
+++ CR_AlgorithmStudy/Cho_Algorithm/Cho_Baekjoon_1StringPrefixSum.cpp
@@ -128,3 +128,27 @@
 //
 //    cout << sum;
 //}
+
+// 04_팰린드롬인지 확인하기
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//    string s;
+//    cin >> s;
+//
+//    int size = s.size();
+//
+//    for (int i = 0; i < size / 2; i++)
+//    {
+//        if (s[i] != s[size - i - 1])
+//        {
+//            cout << 0;
+//            return 0;
+//        }
+//    }
+//
+//    cout << 1;
+//}
