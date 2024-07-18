90516985942fd30fadc468c310e6042b33161aff
ChowonKim <97011721+chowon986@users.noreply.github.com>
2024-07-18 13:26:46 +0900

    Cho : 프로그래머스 Lv2.JadenCase 문자열 만들기

--- CR_AlgorithmStudy/Cho_Algorithm/Cho_Programmers_Lv2.cpp
+++ CR_AlgorithmStudy/Cho_Algorithm/Cho_Programmers_Lv2.cpp
@@ -82,22 +82,62 @@
 
 // 최솟값 만들기
 // https://school.programmers.co.kr/learn/courses/30/lessons/12941
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//int solution(vector<int> A, vector<int> B)
+//{
+//    sort(A.begin(), A.end());
+//    sort(B.begin(), B.end(), greater<>());
+//
+//    int size = A.size();
+//    int answer = 0;
+//    for (int i = 0; i < size; i++)
+//    {
+//        answer += A[i] * B[i];
+//    }
+//
+//    return answer;
+//}
 
-int solution(vector<int> A, vector<int> B)
-{
-    sort(A.begin(), A.end());
-    sort(B.begin(), B.end(), greater<>());
-
-    int size = A.size();
-    int answer = 0;
-    for (int i = 0; i < size; i++)
-    {
-        answer += A[i] * B[i];
-    }
-
-    return answer;
-}
+// JadenCase 문자열 만들기
+// https://school.programmers.co.kr/learn/courses/30/lessons/12951
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//string solution(string s)
+//{
+//    int size = s.size();
+//
+//    bool upper = true;
+//    for (int i = 0; i < size; i++)
+//    {
+//        if (isdigit(s[i]))
+//        {
+//            upper = false;
+//            continue;
+//        }
+//
+//        if (s[i] == ' ')
+//        {
+//            upper = true;
+//            continue;
+//        }
+//
+//        if (upper)
+//        {
+//            s[i] = toupper(s[i]);
+//            upper = false;
+//        }
+//        else
+//        {
+//            s[i] = tolower(s[i]);
+//        }
+//    }
+//
+//    return s;
+//}
