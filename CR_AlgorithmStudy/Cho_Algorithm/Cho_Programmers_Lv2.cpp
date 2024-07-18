afa1fdb8121b81c10f8812dbc01dbd3e35d3d876
ChowonKim <97011721+chowon986@users.noreply.github.com>
2024-07-18 13:10:33 +0900

    Cho : 프로그래머스 Lv2.올바른 괄호

--- CR_AlgorithmStudy/Cho_Algorithm/Cho_Programmers_Lv2.cpp
+++ CR_AlgorithmStudy/Cho_Algorithm/Cho_Programmers_Lv2.cpp
@@ -1,44 +1,81 @@
 // ÃÖ´ñ°ª°ú ÃÖ¼Ú°ª
 // https://school.programmers.co.kr/learn/courses/30/lessons/12939
+//#include <string>
+//#include <vector>
+//#include <sstream>
+//#include <iostream>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//vector<int> SplitString(string& s)
+//{
+//    istringstream iss(s);
+//    string token;
+//    vector<int> tokens;
+//
+//    while (iss >> token)
+//    {
+//        tokens.push_back(stoi(token));
+//    }
+//
+//    return tokens;
+//}
+//
+//string solution(string s) {
+//    vector<int> v = SplitString(s);
+//
+//    sort(v.begin(), v.end());
+//
+//    int minValue = v[0];
+//    int maxValue = v[v.size() - 1];
+//
+//    string answer = "";
+//    answer = to_string(minValue) + ' ' + to_string(maxValue);
+//
+//    return answer;
+//}
+//
+//int main()
+//{
+//    string s = "1 2 3 4";
+//    cout << solution(s);
+//}
 
+// ¿Ã¹Ù¸¥ °ýÈ£
+// https://school.programmers.co.kr/learn/courses/30/lessons/12909
 #include <string>
-#include <vector>
-#include <sstream>
 #include <iostream>
-#include <algorithm>
+#include <stack>
 
 using namespace std;
 
-vector<int> SplitString(string& s)
+bool solution(string s)
 {
-    istringstream iss(s);
-    string token;
-    vector<int> tokens;
+    bool answer = true;
+    stack<char> st;
 
-    while (iss >> token)
+    int size = s.size();
+
+    for (int i = 0; i < size; i++)
     {
-        tokens.push_back(stoi(token));
+        if (s[i] == '(')
+        {
+            st.push(s[i]);
+        }
+        else
+        {
+            if (st.size() > 0)
+            {
+                st.pop();
+            }
+            else
+            {
+                answer = false;
+                return answer;
+            }
+        }
     }
 
-    return tokens;
-}
-
-string solution(string s) {
-    vector<int> v = SplitString(s);
-
-    sort(v.begin(), v.end());
-
-    int minValue = v[0];
-    int maxValue = v[v.size() - 1];
-
-    string answer = "";
-    answer = to_string(minValue) + ' ' + to_string(maxValue);
-
-    return answer;
-}
-
-int main()
-{
-    string s = "1 2 3 4";
-    cout << solution(s);
+    return st.size() == 0 ? answer : !answer;
 }
