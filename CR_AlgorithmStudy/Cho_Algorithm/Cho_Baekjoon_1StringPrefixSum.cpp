2da0cb766ad4202e4a2eb64b37dae45f2b7856ad
ChowonKim <97011721+chowon986@users.noreply.github.com>
2024-07-25 18:19:03 +0900

    Cho : 백준 1.문자열,누적합(수열)

--- CR_AlgorithmStudy/Cho_Algorithm/Cho_Baekjoon_1StringPrefixSum.cpp
+++ CR_AlgorithmStudy/Cho_Algorithm/Cho_Baekjoon_1StringPrefixSum.cpp
@@ -392,54 +392,88 @@
 
 // 한국이 그리울 땐 서버에 접속하지
 // https://www.acmicpc.net/problem/9996
+//#include <iostream>
+//#include <vector>
+//#include <sstream>
+//using namespace std;
+//
+//vector<string> splitString(string& s, char d)
+//{
+//    istringstream iss(s);
+//    string token;
+//    vector<string> tokens;
+//
+//    while (getline(iss, token, d))
+//    {
+//        tokens.push_back(token);
+//    }
+//
+//    return tokens;
+//}
+//
+//int main()
+//{
+//    int cnt;
+//    cin >> cnt;
+//
+//    string s;
+//    cin >> s;
+//    vector<string> v = splitString(s, '*');
+//
+//    for (int i = 0; i < cnt; i++)
+//    {
+//        string temp;
+//        cin >> temp;
+//
+//        if (temp.size() < v[0].size() + v[1].size())
+//        {
+//            cout << "NE" << '\n';
+//            continue;
+//        }
+//
+//        string start = temp.substr(0, v[0].size());
+//        string end = temp.substr(temp.size() - v[1].size(), temp.size());
+//        if (start == v[0] && end == v[1])
+//        {
+//            cout << "DA" << '\n';
+//        }
+//        else
+//        {
+//            cout << "NE" << '\n';
+//        }
+//    }
+//}
+
+// 수열
+// https://www.acmicpc.net/problem/2559
 #include <iostream>
-#include <vector>
-#include <sstream>
+#include <queue>
+#include <math.h>
 using namespace std;
-
-vector<string> splitString(string& s, char d)
-{
-    istringstream iss(s);
-    string token;
-    vector<string> tokens;
-
-    while (getline(iss, token, d))
-    {
-        tokens.push_back(token);
-    }
-
-    return tokens;
-}
 
 int main()
 {
-    int cnt;
-    cin >> cnt;
+    int n, m;
+    cin >> n >> m;
 
-    string s;
-    cin >> s;
-    vector<string> v = splitString(s, '*');
-
-    for (int i = 0; i < cnt; i++)
+    queue<int> q;
+    int sum = 0;
+    int maxValue = -100 * n;
+    for (int i = 0; i < n; i++)
     {
-        string temp;
+        int temp;
         cin >> temp;
 
-        if (temp.size() < v[0].size() + v[1].size())
-        {
-            cout << "NE" << '\n';
-            continue;
-        }
+        q.push(temp);
+        sum += temp;
 
-        string start = temp.substr(0, v[0].size());
-        string end = temp.substr(temp.size() - v[1].size(), temp.size());
-        if (start == v[0] && end == v[1])
+        if (q.size() == m)
         {
-            cout << "DA" << '\n';
-        }
-        else
-        {
-            cout << "NE" << '\n';
+            maxValue = max(maxValue, sum);
+            sum -= q.front();
+            q.pop();
         }
     }
+
+    cout << maxValue;
 }
