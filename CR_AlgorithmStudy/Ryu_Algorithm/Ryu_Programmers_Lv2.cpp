ba75c1c58c6019ddd9c451366e0bbced392fe0ac
Rotus <12rotus31@gmail.com>
2024-08-16 14:47:52 +0900

    Ryu : 프로그래머스 Lv2.타겟 넘버

--- CR_AlgorithmStudy/Ryu_Algorithm/Ryu_Programmers_Lv2.cpp
+++ CR_AlgorithmStudy/Ryu_Algorithm/Ryu_Programmers_Lv2.cpp
@@ -3,32 +3,32 @@
 using namespace std;
 
 // 완전탐색_Lv2 피로도
-bool visited[10];
-int cnt = -1;
-
-void Enter(int energy, int count, const vector<vector<int>>& dungeons)
-{
-    // 던전 최대 방문수 업데이트
-    cnt = max(cnt, count);
-
-    for (int i = 0; i < dungeons.size(); ++i)
-    {
-        // 방문 했으면 무시
-        if (true == visited[i]) continue;
-
-        // 최소 필요 피로도가 되는 경우
-        if (dungeons[i][0] <= energy)
-        {
-            // 방문 기록
-            visited[i] = true;
-            // 던전 탐사
-            Enter(energy - dungeons[i][1], count + 1, dungeons);
-            // 방문 복원
-            visited[i] = false;
-        }
-    }
-}
-
+//bool visited[10];
+//int cnt = -1;
+//
+//void Enter(int energy, int count, const vector<vector<int>>& dungeons)
+//{
+//    // 던전 최대 방문수 업데이트
+//    cnt = max(cnt, count);
+//
+//    for (int i = 0; i < dungeons.size(); ++i)
+//    {
+//        // 방문 했으면 무시
+//        if (true == visited[i]) continue;
+//
+//        // 최소 필요 피로도가 되는 경우
+//        if (dungeons[i][0] <= energy)
+//        {
+//            // 방문 기록
+//            visited[i] = true;
+//            // 던전 탐사
+//            Enter(energy - dungeons[i][1], count + 1, dungeons);
+//            // 방문 복원
+//            visited[i] = false;
+//        }
+//    }
+//}
+//
 //int solution(int k, vector<vector<int>> dungeons)
 //{
 //    Enter(k, 0, dungeons);
@@ -38,35 +38,61 @@ void Enter(int energy, int count, const vector<vector<int>>& dungeons)
 
 // 완전탐색_Lv2 모음 사전
 // 이렇게 푸는게 아닌거 같기도.. 시간이 에법 오래 걸림
-char ch[5] = { 'A', 'E', 'I', 'O', 'U' };
-int cnt;
-bool bIsFind = false;
+//char ch[5] = { 'A', 'E', 'I', 'O', 'U' };
+//int cnt;
+//bool bIsFind = false;
+//
+//void check(const string& word, string str, int depth)
+//{
+//    // 길이 확인
+//    if (5 < depth) return;
+//    // 동일 문자 확인
+//    if (word == str)
+//    {
+//        bIsFind = true;
+//        return;
+//    }
+//
+//    ++cnt;
+//
+//    for (int i = 0; i < 5; ++i)
+//    {
+//        // 동일 문자 찾았을 시 순회 탈출
+//        if (true == bIsFind) break;
+//
+//        check(word, str + ch[i], depth + 1);
+//    }
+//}
+//
+//int solution(string word)
+//{
+//    check(word, "", 0);
+//
+//    return cnt;
+//}
 
-void check(const string& word, string str, int depth)
+// DFS/BFS_Lv2 타겟 넘버
+int cnt;
+
+void cal(const vector<int>& numbers, const int& target, int num, int index)
 {
-    // 길이 확인
-    if (5 < depth) return;
-    // 동일 문자 확인
-    if (word == str)
+    // 기저사례(마지막 확인)
+    if (numbers.size() == index)
     {
-        bIsFind = true;
+        // 타겟과 동일하면 카운트 증가
+        if (target == num) ++cnt;
         return;
     }
 
-    ++cnt;
-
-    for (int i = 0; i < 5; ++i)
-    {
-        // 동일 문자 찾았을 시 순회 탈출
-        if (true == bIsFind) break;
-
-        check(word, str + ch[i], depth + 1);
-    }
+    // 양수
+    cal(numbers, target, num + numbers[index], index + 1);
+    // 음수
+    cal(numbers, target, num - numbers[index], index + 1);
 }
 
-int solution(string word)
+int solution(vector<int> numbers, int target)
 {
-    check(word, "", 0);
+    cal(numbers, target, 0, 0);
 
     return cnt;
 }
@@ -79,10 +105,14 @@ int main(void)
     //solution(100, { {100, 1},{99, 1},{99, 1},{99, 1},{99, 1},{99, 1},{99, 1} });    // 2
 
     // 완전탐색_Lv2 모음 사전
-    solution("AAAAE");      // 6
-    solution("AAAE");       // 10
-    solution("I");          // 1563
-    solution("EIO");        // 1189
+    //solution("AAAAE");      // 6
+    //solution("AAAE");       // 10
+    //solution("I");          // 1563
+    //solution("EIO");        // 1189
+
+    // DFS/BFS_Lv2 타겟 넘버
+    solution({ 1, 1, 1, 1, 1 }, 3);     // 5
+    solution({ 4, 1, 2, 1 }, 4);        // 2
 
     return 0;
 }
