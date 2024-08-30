5bd91b4610fabc944fbb85cf45a6454ac063f426
ChowonKim <97011721+chowon986@users.noreply.github.com>
2024-08-30 16:51:16 +0900

    Cho : 백준 2.DFS,BFS(유기농 배추)

--- CR_AlgorithmStudy/Cho_Algorithm/Cho_Baekjoon_2DFSBFS.cpp
+++ CR_AlgorithmStudy/Cho_Algorithm/Cho_Baekjoon_2DFSBFS.cpp
@@ -1,5 +1,66 @@
 // ¹Ì·Î Å½»ö
 // https://www.acmicpc.net/problem/2178
+//#include <iostream>
+//#include <vector>
+//#include <queue>
+//using namespace std;
+//
+//int dx[4] = { 1, -1, 0, 0 };
+//int dy[4] = { 0, 0, 1, -1 };
+//
+//int bfs(vector<vector<char>>& v, int n, int m)
+//{
+//    queue<pair<int, int>> q;
+//    q.push({ 0, 0 });
+//    vector<vector<bool>> isVisited(n, vector<bool>(m, false));
+//    vector<vector<int>> visitedCnt(n, vector<int>(m, 0));
+//    isVisited[0][0] = true;
+//    visitedCnt[0][0] = 1;
+//
+//    while (!q.empty())
+//    {
+//        int x = q.front().first;
+//        int y = q.front().second;
+//        q.pop();
+//
+//        for (int i = 0; i < 4; i++)
+//        {
+//            int nx = x + dx[i];
+//            int ny = y + dy[i];
+//
+//            if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+//            if (isVisited[nx][ny] || v[nx][ny] == '0') continue;
+//            isVisited[nx][ny] = true;
+//            visitedCnt[nx][ny] = visitedCnt[x][y] + 1;
+//            q.push({ nx, ny });
+//        }
+//    }
+//
+//    return visitedCnt[n - 1][m - 1];
+//}
+//
+//int main()
+//{
+//    int n, m;
+//    cin >> n >> m;
+//    vector<vector<char>> v(n, vector<char>(m));
+//
+//    for (int i = 0; i < n; i++)
+//    {
+//        string temp;
+//        cin >> temp;
+//
+//        for (int j = 0; j < m; j++)
+//        {
+//            v[i][j] = temp[j];
+//        }
+//    }
+//
+//    cout << bfs(v, n, m);
+//}
+
+// À¯±â³ó¹èÃß
+// https://www.acmicpc.net/problem/1012
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -8,53 +69,60 @@ using namespace std;
 int dx[4] = { 1, -1, 0, 0 };
 int dy[4] = { 0, 0, 1, -1 };
 
-int bfs(vector<vector<char>>& v, int n, int m)
+void bfs(vector<vector<bool>>& v, int x, int y, int mx, int my)
 {
     queue<pair<int, int>> q;
-    q.push({ 0, 0 });
-    vector<vector<bool>> isVisited(n, vector<bool>(m, false));
-    vector<vector<int>> visitedCnt(n, vector<int>(m, 0));
-    isVisited[0][0] = true;
-    visitedCnt[0][0] = 1;
+    q.push({ x, y });
 
     while (!q.empty())
     {
-        int x = q.front().first;
-        int y = q.front().second;
+        int cx = q.front().first;
+        int cy = q.front().second;
         q.pop();
 
         for (int i = 0; i < 4; i++)
         {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+            int nx = cx + dx[i];
+            int ny = cy + dy[i];
 
-            if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-            if (isVisited[nx][ny] || v[nx][ny] == '0') continue;
-            isVisited[nx][ny] = true;
-            visitedCnt[nx][ny] = visitedCnt[x][y] + 1;
+            if (nx < 0 || nx >= mx || ny < 0 || ny >= my) continue;
+            if (v[nx][ny] == false) continue;
+            v[nx][ny] = false;
             q.push({ nx, ny });
         }
     }
-
-    return visitedCnt[n - 1][m - 1];
 }
 
 int main()
 {
-    int n, m;
-    cin >> n >> m;
-    vector<vector<char>> v(n, vector<char>(m));
+    int tc;
+    cin >> tc;
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < tc; i++)
     {
-        string temp;
-        cin >> temp;
-
-        for (int j = 0; j < m; j++)
+        int x, y, z;
+        cin >> x >> y >> z;
+        vector<vector<bool>> v(x, vector<bool>(y, false));
+        for (int i = 0; i < z; i++)
         {
-            v[i][j] = temp[j];
+            int tx, ty;
+            cin >> tx >> ty;
+            v[tx][ty] = true;
         }
-    }
 
-    cout << bfs(v, n, m);
+        int cnt = 0;
+        for (int i = 0; i < x; i++)
+        {
+            for (int j = 0; j < y; j++)
+            {
+                if (v[i][j] == true)
+                {
+                    bfs(v, i, j, x, y);
+                    ++cnt;
+                }
+            }
+        }
+
+        cout << cnt << '\n';
+    }
 }
