f71aedba51014538401e34a66b4b1b738b847d79
Rotus <12rotus31@gmail.com>
2024-09-03 14:09:29 +0900

    Ryu : 백준 4.비트마스킹(게리맨더링_인접리스트, DFS(CC))

--- CR_AlgorithmStudy/Ryu_Algorithm/Ryu_Baekjoon_4BitMasking.cpp
+++ CR_AlgorithmStudy/Ryu_Algorithm/Ryu_Baekjoon_4BitMasking.cpp
@@ -86,66 +86,152 @@ using namespace std;
 // 행을 뒤집으면 열을 뒤집는다는 것은 최적해가 이미 정해진 것(반대로도 마찬가지)
 // 따라서 2의 40승이 아닌 20승으로 행만 뒤집고 각 열을 확인하며 뒤집을지 여부 판단
 // 어려와요,,,ㅠ 몰루게따ㅋㅎ
-int n, coinTable[21], ret = 987654321;
-int dy[] = { -1, 0, 1, 0 };
-int dx[] = { 0, 1, 0, -1 };
+//int n, coinTable[21], ret = 987654321;
+//int dy[] = { -1, 0, 1, 0 };
+//int dx[] = { 0, 1, 0, -1 };
+//
+//void go(int here) 
+//{
+//	// 기저사례(행만 뒤집고 열은 판단해서 뒤집기)
+//	if (here == n + 1) 
+//	{
+//		int sum = 0;
+//		// 1, 2, 4, 8,.. 확인
+//		for (int i = 1; i <= (1 << (n - 1)); i *= 2) 
+//		{
+//			int cnt = 0;
+//			// 특정 행들의 부분 요소를 확인하기 때문에 ~n = -(n + 1)으로 비트마스킹의 앞부분 고려 X
+//			for (int j = 1; j <= n; ++j)
+//			{
+//				// coinTable[j]: 행
+//				// 즉 하나의 행 부분 요소 확인
+//				if (coinTable[j] & i) ++cnt;
+//			}
+//
+//			// 최소 및 윗면 합 연산
+//			sum += min(cnt, n - cnt);
+//		}
+//
+//		ret = min(ret, sum);
+//		return;
+//	}
+//
+//	// 뒤집기 X
+//	go(here + 1);
+//
+//	// 뒤집기
+//	coinTable[here] = ~coinTable[here];
+//	// 뒤집기 O
+//	go(here + 1);
+//}
+//
+//int main() 
+//{
+//	cin >> n;
+//	for (int i = 1; i <= n; ++i) 
+//	{
+//		string coinInfo;
+//		cin >> coinInfo;
+//
+//		int value = 1;
+//		for (int j = 0; j < n; ++j) 
+//		{
+//			// 각 행(문자열)을 숫자로 표현(HHT -> 4, TTT -> 7, HHH -> 0)
+//			if ('T' == coinInfo[j]) coinTable[i] |= value;
+//
+//			value *= 2;
+//		}
+//	}
+//
+//	go(1);
+//	
+//	cout << ret;
+//	
+//	return 0;
+//}
 
-void go(int here) 
+// 3_게리맨더링 
+// https://www.acmicpc.net/problem/17471
+// 노드마다 색칠 할 것, 색칠해서 2가지로 나누어져야함 3 이상으로 나누어지면 X
+// 모든 경우의 수를 확인하여 그 중 2개로 나누어진 것 중에서 최솟값을 갱신
+// 각 구역들을 2개의 큰 영역으로 나누어 영역별 인구수의 차가 가장 적은 경우의 차이 최솟값
+// n: 구역 개수
+const int VAL = 987654321;
+int n, m, ret = VAL, area[11], comp[11], visited[11];
+vector<int> adj[11];
+
+pair<int, int> dfs(int here, int value) 
 {
-	// 기저사례(행만 뒤집고 열은 판단해서 뒤집기)
-	if (here == n + 1) 
-	{
-		int sum = 0;
-		// 1, 2, 4, 8,.. 확인
-		for (int i = 1; i <= (1 << (n - 1)); i *= 2) 
-		{
-			int cnt = 0;
-			// 특정 행들의 부분 요소를 확인하기 때문에 ~n = -(n + 1)으로 비트마스킹의 앞부분 고려 X
-			for (int j = 1; j <= n; ++j)
-			{
-				// coinTable[j]: 행
-				// 즉 하나의 행 부분 요소 확인
-				if (coinTable[j] & i) ++cnt;
-			}
+    visited[here] = 1;
+                       // 노드 자식의 수, 인구 수
+    pair<int, int> ret = { 1, area[here] };
+    for (int there : adj[here]) 
+    {
+        // 색이 다르면 무시
+        if (comp[there] != value) continue;
+        // 방문한 적이 있다면 무시
+        if (0 != visited[there]) continue;
 
-			// 최소 및 윗면 합 연산
-			sum += min(cnt, n - cnt);
-		}
+        pair<int, int> tmp = dfs(there, value);
+        ret.first += tmp.first;
+        ret.second += tmp.second;
+    }
 
-		ret = min(ret, sum);
-		return;
-	}
-
-	// 뒤집기 X
-	go(here + 1);
-
-	// 뒤집기
-	coinTable[here] = ~coinTable[here];
-	// 뒤집기 O
-	go(here + 1);
+    return ret;
 }
 
 int main() 
 {
-	cin >> n;
-	for (int i = 1; i <= n; ++i) 
-	{
-		string coinInfo;
-		cin >> coinInfo;
+    // 구역 수 입력 받기
+    cin >> n;
+    // 구역당 인구 수 입력 받기
+    for (int i = 1; i <= n; ++i) cin >> area[i];
+    for (int i = 1; i <= n; ++i) 
+    {
+        // 구역 당 다른 구역과 연결 된 수 입력 받기
+        cin >> m;
+        // 어느 구역과 연결되어 있는지 입력 받기 & 연결 작업
+        for (int j = 0; j < m; ++j) 
+        {
+            int temp = 0;
+            cin >> temp;
 
-		int value = 1;
-		for (int j = 0; j < n; ++j) 
-		{
-			// 각 행(문자열)을 숫자로 표현(HHT -> 4, TTT -> 7, HHH -> 0)
-			if ('T' == coinInfo[j]) coinTable[i] |= value;
+            // 인접 리스트
+            adj[i].push_back(temp);
+            adj[temp].push_back(i);
+        }
+    }
 
-			value *= 2;
-		}
-	}
+    // 모든 경우의 수 확인
+    for (int i = 1; i < (1 << n) - 1; ++i) 
+    {
+        // 초기화 작업
+        fill(comp, comp + 11, 0);
+        fill(visited, visited + 11, 0);
 
-	go(1);
-	
-	cout << ret;
-	
-	return 0;
+        int a = -1;
+        int b = -1;
+        for (int j = 0; j < n; ++j) 
+        {
+            if (i & (1 << j)) 
+            { 
+                // 색칠 작업
+                comp[j + 1] = 1; 
+                // A 시작 지점
+                a = j + 1; 
+            }
+            // B 시작 지점
+            else b = j + 1;
+        }
+
+        pair<int, int> A = dfs(a, 1);
+        pair<int, int> B = dfs(b, 0);
+
+        // n == 합 을 하는 이유: 3개 이상으로 쪼개지는지 확인하기 위함(3개 이상이면 둘을 합했을 때 n보다 작음)
+        if (n == A.first + B.first) ret = min(ret, abs(A.second - B.second));
+    }
+
+    cout << (VAL == ret ? -1 : ret);
+
+    return 0;
 }
