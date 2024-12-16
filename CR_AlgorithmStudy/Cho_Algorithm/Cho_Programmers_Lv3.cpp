4c183a73746c682023a1acb261dd5e07ea8f7792
ChowonKim <97011721+chowon986@users.noreply.github.com>
2024-12-16 18:19:12 +0900

    Cho : 프로그래머스 Lv3.부대복귀

--- CR_AlgorithmStudy/Cho_Algorithm/Cho_Programmers_Lv3.cpp
+++ CR_AlgorithmStudy/Cho_Algorithm/Cho_Programmers_Lv3.cpp
@@ -110,6 +110,69 @@
 // [카카오 인턴] 경주로 건설
 // https://school.programmers.co.kr/learn/courses/30/lessons/67259
 
+//#include <string>
+//#include <vector>
+//#include <queue>
+//#include <limits.h>
+//#include <iostream>
+//
+//using namespace std;
+//
+//int dx[4] = { 0, 1, -1, 0 };
+//int dy[4] = { 1, 0, 0, -1 };
+//
+//struct Node
+//{
+//    int x, y, direction, cost;
+//};
+//
+//int solution(vector<vector<int>> board) 
+//{
+//    vector<vector<vector<int>>> cost(board.size(), vector<vector<int>>(board.size(), vector<int>(4, INT_MAX)));
+//    queue<Node> q;
+//    q.push({ 0, 0, -1, 0 });
+//
+//    for (int i = 0; i < 4; i++)
+//    {
+//        cost[0][0][i] = 0;
+//    }
+//
+//    while (!q.empty())
+//    {
+//        Node curNode = q.front();
+//        q.pop();
+//
+//        for (int i = 0; i < 4; i++)
+//        {
+//            int nx = curNode.x + dx[i];
+//            int ny = curNode.y + dy[i];
+//            int newCost = curNode.cost;
+//
+//            if (nx < 0 || ny < 0 || nx >= board.size() || ny >= board.size() || board[nx][ny] == 1) continue;
+//
+//            if (curNode.direction == -1 || curNode.direction == i) newCost += 100;
+//            else newCost += 600;
+//
+//            if (newCost < cost[nx][ny][i])
+//            {
+//                cost[nx][ny][i] = newCost;
+//                q.push({ nx, ny, i, newCost });
+//            }
+//        }
+//    }
+//
+//    int answer = INT_MAX;
+//    for (int i = 0; i < 4; i++)
+//    {
+//        answer = min(answer, cost[board.size() - 1][board.size() - 1][i]);
+//    }
+//
+//    return answer;
+//}
+
+// 부대복귀
+// https://school.programmers.co.kr/learn/courses/30/lessons/132266?language=cpp
+
 #include <string>
 #include <vector>
 #include <queue>
@@ -118,53 +181,58 @@
 
 using namespace std;
 
-int dx[4] = { 0, 1, -1, 0 };
-int dy[4] = { 1, 0, 0, -1 };
-
-struct Node
+vector<int> dijkstra(int n, vector<vector<pair<int, int>>>& road, int start)
 {
-    int x, y, direction, cost;
-};
+    vector<int> dist(n + 1, INT_MAX);
+    dist[start] = 0;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({ 0, start });
 
-int solution(vector<vector<int>> board) 
-{
-    vector<vector<vector<int>>> cost(board.size(), vector<vector<int>>(board.size(), vector<int>(4, INT_MAX)));
-    queue<Node> q;
-    q.push({ 0, 0, -1, 0 });
-
-    for (int i = 0; i < 4; i++)
+    while (!pq.empty())
     {
-        cost[0][0][i] = 0;
-    }
+        int distance = pq.top().first;
+        int curNode = pq.top().second;
 
-    while (!q.empty())
-    {
-        Node curNode = q.front();
-        q.pop();
+        pq.pop();
 
-        for (int i = 0; i < 4; i++)
+        if (distance > dist[curNode]) continue;
+
+        for (auto node : road[curNode])
         {
-            int nx = curNode.x + dx[i];
-            int ny = curNode.y + dy[i];
-            int newCost = curNode.cost;
+            int nextNode = node.first;
+            int weight = node.second;
 
-            if (nx < 0 || ny < 0 || nx >= board.size() || ny >= board.size() || board[nx][ny] == 1) continue;
-
-            if (curNode.direction == -1 || curNode.direction == i) newCost += 100;
-            else newCost += 600;
-
-            if (newCost < cost[nx][ny][i])
+            if (dist[curNode] + weight < dist[nextNode])
             {
-                cost[nx][ny][i] = newCost;
-                q.push({ nx, ny, i, newCost });
+                dist[nextNode] = dist[curNode] + weight;
+                pq.push({ dist[nextNode], nextNode });
             }
         }
     }
 
-    int answer = INT_MAX;
-    for (int i = 0; i < 4; i++)
+    return dist;
+}
+
+vector<int> solution(int n, vector<vector<int>> roads, vector<int> sources, int destination)
+{
+    // 목적지에서 각 sources의 거리를 구하면 됨 -> 다익스트라
+    vector<vector<pair<int, int>>> road(n + 1);
+
+    for (int i = 0; i < roads.size(); i++)
     {
-        answer = min(answer, cost[board.size() - 1][board.size() - 1][i]);
+        int from = roads[i][0];
+        int to = roads[i][1];
+        road[from].push_back({ to, 1 });
+        road[to].push_back({ from, 1 });
+    }
+
+    vector<int> result = dijkstra(n, road, destination);
+    vector<int> answer;
+
+    for (auto to : sources)
+    {
+        int value = result[to] == INT_MAX ? -1 : result[to];
+        answer.push_back(value);
     }
 
     return answer;
